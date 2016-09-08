c3c93d08c8e34da0d7477fae067d0d28a420d74a
kaiziwangzi <kaiziwangzi@gmail.com>
2016-09-08 11:27:27 +0800

    the base round view

---
+++ app/src/main/java/com/brook/viewpagerindicator/RoundView.java
@@ -0,0 +1,23 @@
+package com.brook.viewpagerindicator;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Created by Brook on 16/9/8.
+ * Description:
+ */
+public class RoundView extends View {
+    public RoundView(Context context) {
+        this(context, null);
+    }
+
+    public RoundView(Context context, AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public RoundView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+}

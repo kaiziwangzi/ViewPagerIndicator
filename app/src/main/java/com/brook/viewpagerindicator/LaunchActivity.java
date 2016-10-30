7b9cebf621dc59e54a69701e0701f7983228e922
kaiziwangzi <kaiziwangzi@gmail.com>
2016-10-30 20:50:24 +0800

    simple demo

--- app/src/main/java/com/brook/viewpagerindicator/LaunchActivity.java
+++ app/src/main/java/com/brook/viewpagerindicator/LaunchActivity.java
@@ -6,20 +6,20 @@ import android.view.View;
 
 public class LaunchActivity extends AppCompatActivity {
 
-    private RoundView roundView;
+//    private RoundView roundView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_launch);
 
-        roundView = (RoundView) findViewById(R.id.roundView);
+//        roundView = (RoundView) findViewById(R.id.roundView);
 //        roundView.notifyRound();
-        findViewById(R.id.start).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                roundView.notifyRound();
-            }
-        });
+//        findViewById(R.id.start).setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                roundView.notifyRound();
+//            }
+//        });
     }
 }
--- app/src/main/java/com/brook/viewpagerindicator/view/IndicatorView.java
+++ app/src/main/java/com/brook/viewpagerindicator/view/IndicatorView.java
@@ -18,11 +18,14 @@ import com.brook.viewpagerindicator.util.DensityUtils;
 public class IndicatorView extends View {
     private static final int DEFAULT_RADIUS_DP = 5;
     private static final int DEFAULT_PADDING_DP = 10;
+    private static final int DEFAULT_SELECTED_COLOR = 0xfffffff;
+    private static final int DEFAULT_UNSELECTED_COLOR = 0x33ffffff;
 
     private int mRadiusPx = DensityUtils.dp2px(getContext(), DEFAULT_RADIUS_DP);
     private int mPaddingPx = DensityUtils.dp2px(getContext(), DEFAULT_PADDING_DP);
     private int mSelectedPosition;
     private Paint mPaint;
+    private int count = 8;
 
     public IndicatorView(Context context) {
         super(context);
@@ -58,10 +61,61 @@ public class IndicatorView extends View {
     @Override
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
         super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+        int widthMode = MeasureSpec.getMode(widthMeasureSpec);
+        int widthSize = MeasureSpec.getSize(widthMeasureSpec);
+        int heightMode = MeasureSpec.getMode(heightMeasureSpec);
+        int heightSize = MeasureSpec.getSize(heightMeasureSpec);
+
+        int width;
+        int height;
+        int diameterPx = mRadiusPx * 2;
+        int desiredWidth = 0;
+        int desiredHeight = diameterPx;
+        if (0 != count) {
+            desiredWidth = (count * diameterPx) + (mPaddingPx * (count - 1));
+
+        }
+        if (widthMode == MeasureSpec.EXACTLY) {
+            width = widthSize;
+        } else if (widthMode == MeasureSpec.AT_MOST) {
+            width = Math.min(widthSize, desiredWidth);
+        } else {
+            width = desiredWidth;
+        }
+
+        if (heightMode == MeasureSpec.EXACTLY) {
+            height = heightSize;
+        } else if (heightMode == MeasureSpec.AT_MOST) {
+            height = Math.min(heightSize, desiredHeight);
+        } else {
+            height = desiredHeight;
+        }
+
+        setMeasuredDimension(width, height);
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
+        drawIndicatorView(canvas);
+    }
+
+    private void drawIndicatorView(Canvas canvas) {
+        int y = getCalculateHeight() / 2;
+        for (int i = 0; i < count; i++) {
+            canvas.drawCircle(getPositionX(i), y, mRadiusPx, mPaint);
+        }
+    }
+
+    private int getPositionX(int position) {
+        return mRadiusPx + (mRadiusPx * 2 + mPaddingPx) * position;
+    }
+
+    private int getCalculateWidth() {
+        return 0;
+    }
+
+    private int getCalculateHeight() {
+        return getHeight();
     }
 }
